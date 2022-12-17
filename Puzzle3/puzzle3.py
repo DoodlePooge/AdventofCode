76a0992c6660a0e95a88ea29a8f606c55a8dfdc7
Paige Maute <p.maute@obscuritylabs.com>
2022-12-17 01:48:49 -0500

    puzzle 3 complete

--- Puzzle3/puzzle3.py
+++ Puzzle3/puzzle3.py
@@ -1,9 +1,17 @@
-def find_the_type(left: str, right: str) -> str:
+def find_the_type_pt_one(left: str, right: str) -> str:
     type = ''
     for l in left:
         for r in right:
             if l == r:
                 type = r
+    return type
+
+def find_the_type_pt_two(rucksacks: list[str]) -> str:
+    type = ''
+    for f in rucksacks[0]:
+        if rucksacks[1].find(f) >= 0:
+            if rucksacks[2].find(f) >=0:
+                type = f
     return type
 
 def determine_priority(type: str) -> int:
@@ -16,7 +24,7 @@ def priority_sum(priorities: list[int]) -> int:
         sum += priority
     return sum
 
-def traverse_file(fileName: str) -> list[int]:
+def traverse_file_pt_one(fileName: str) -> list[int]:
     file = open(fileName)
     lines = file.readlines()
     priorities: list[int] = []
@@ -26,12 +34,33 @@ def traverse_file(fileName: str) -> list[int]:
         mid = len(line) // 2
         left = line[:mid]
         right = line[mid:]
-        priorities.append(determine_priority(find_the_type(left, right)))
+        priorities.append(determine_priority(find_the_type_pt_one(left, right)))
+
+    return priorities
+
+def traverse_file_pt_two(fileName: str) -> list[int]:
+    file = open(fileName)
+    lines = file.readlines()
+    priorities: list[int] = []
+    i = 0
+    rucksacks = ['', '', '']
+
+    for line in lines:
+        line = line.strip()
+        rucksacks[i] = line
+        if i == 2:
+            priorities.append(determine_priority(find_the_type_pt_two(rucksacks)))
+            i = 0
+        else:
+            i += 1
 
     return priorities
 
 def main():
-    priorities = traverse_file('input3.txt')
+    priorities = traverse_file_pt_one('input3.txt')
+    print(priority_sum(priorities))
+
+    priorities = traverse_file_pt_two('input3.txt')
     print(priority_sum(priorities))
 
 main()
--- Puzzle3/test_puzzle3.py
+++ Puzzle3/test_puzzle3.py
@@ -1,17 +1,26 @@
-from puzzle3 import find_the_type, traverse_file, determine_priority, priority_sum
+from puzzle3 import determine_priority, priority_sum, traverse_file_pt_one, traverse_file_pt_two, find_the_type_pt_one, find_the_type_pt_two
 
-rucksack = 'vJrwpWtwJgWrhcsFMMfFFhFp'
-priorities = [16, 38, 42, 22, 20, 19]
+rucksacks = ['vJrwpWtwJgWrhcsFMMfFFhFp', 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL', 'PmmdzqPrVvPwwTWBwg']
+priorities1 = [16, 38, 42, 22, 20, 19]
+priorities2 = [18, 52]
 
-def test_find_the_type() -> None:
-    mid = len(rucksack) // 2
-    assert 'p' == find_the_type(rucksack[:mid], rucksack[mid:])
+def test_find_the_type_pt_one() -> None:
+    mid = len(rucksacks[0]) // 2
+    assert 'p' == find_the_type_pt_one(rucksacks[0][:mid], rucksacks[0][mid:])
+
+def test_find_the_type_pt_two() -> None:
+    assert 'r' == find_the_type_pt_two(rucksacks)
 
 def test_determine_priority() -> None:
     assert 16 == determine_priority('p')
+    assert 52 == determine_priority('Z')
 
 def test_priority_sum() -> None:
-    assert 157 == priority_sum(priorities)
+    assert 157 == priority_sum(priorities1)
+    assert 70 == priority_sum(priorities2)
 
-def test_traverse_file() -> None:
-    assert priorities == traverse_file('example_input.txt')
+def test_traverse_file_pt_one() -> None:
+    assert priorities1 == traverse_file_pt_one('example_input.txt')
+
+def test_traverse_file_pt_two() -> None:
+    assert priorities2 == traverse_file_pt_two('example_input.txt')
