c8a1291d371c6e6aad955684e33597f7f75c7fbc
Paige Maute <p.maute@obscuritylabs.com>
2022-12-16 23:43:31 -0500

    puzzle 2 complete

--- Puzzle2/puzzle2.py
+++ Puzzle2/puzzle2.py
@@ -1,6 +1,6 @@
 
 
-def round_score(play: str, outcome: str) -> int:
+def round_score_pt_one(play: str, outcome: str) -> int:
     playScore = 0
     outcomeScore = 0
 
@@ -18,16 +18,70 @@ def round_score(play: str, outcome: str) -> int:
 
     return playScore + outcomeScore
 
+def round_score_pt_two(play: str, outcome: str) -> int:
+    playScore = 0
+    outcomeScore = 0
+
+    if outcome == 'X':
+        outcomeScore = 0
+    elif outcome == 'Y':
+        outcomeScore = 3
+    else:
+        outcomeScore = 6
+
+    if play == 'scissors':
+        playScore = 3
+    elif play == 'paper':
+        playScore = 2
+    else:
+        playScore = 1
+
+    return playScore + outcomeScore
+
 def total_score(rounds: list[int]) -> int:
     sum = 0
     for round in rounds:
         sum += round
     return sum
 
-def traverse_file(fileName: str) -> list[int]:
-    return [0]
+def determine_play(line: str) -> str:
+    if line == 'A X' or line == 'B Z' or line == 'C Y':
+        return 'scissors'
+    elif line == 'A Y' or line == 'B X' or line == 'C Z':
+        return 'rock'
+    return 'paper'
+
+def determine_outcome(line: str) -> str:
+    if line == 'A X' or line == 'B Y' or line == 'C Z':
+        return 'tie'
+    elif line == 'A Y' or line == 'B Z' or line == 'C X':
+        return 'win'
+    return 'loss'
+
+def traverse_file_pt_one(fileName: str) -> list[int]:
+    file = open(fileName)
+    lines = file.readlines()
+    rounds: list[int] = []
+
+    for line in lines:
+        rounds.append(round_score_pt_one(line[-2], determine_outcome(line.strip())))
+    
+    return rounds
+
+def traverse_file_pt_two(fileName: str) -> list[int]:
+    file = open(fileName)
+    lines = file.readlines()
+    rounds: list[int] = []
+
+    for line in lines:
+        rounds.append(round_score_pt_two(determine_play(line.strip()), line[-2]))
+    
+    return rounds
 
 def main():
-    return
+    rounds = traverse_file_pt_one('input2.txt')
+    print(total_score(rounds))
+    rounds = traverse_file_pt_two('input2.txt')
+    print(total_score(rounds))
 
 main()
--- Puzzle2/puzzle2_test.py
+++ Puzzle2/puzzle2_test.py
@@ -1,15 +1,35 @@
-from puzzle2 import round_score, total_score, traverse_file
+from puzzle2 import total_score, traverse_file_pt_one, traverse_file_pt_two, determine_outcome, determine_play, round_score_pt_one, round_score_pt_two
 
-rounds: list[int] = [8, 1, 6]
-calories: list[int] = [7000, 8000, 9000]
+rounds1: list[int] = [8, 1, 6]
+rounds2: list[int] = [4, 1, 7]
     
-def test_round_score() -> None:
-    score = round_score('Y', 'win')
+def test_round_score_pt_one() -> None:
+    score = round_score_pt_one('Y', 'win')
     assert score == 8
 
-def test_add_calories_held() -> None:
-    sum = total_score(rounds)
+def test_round_score_pt_two() -> None:
+    score = round_score_pt_two('scissors', 'Y')
+    assert score == 6
+
+def test_total_score() -> None:
+    sum = total_score(rounds1)
     assert sum == 15
 
-def test_traverse_file() -> None:
-    assert rounds == traverse_file("exampleInput.txt")
+def test_determine_outcome() -> None:
+    outcome = determine_outcome('A Y')
+    assert outcome == 'win'
+
+def test_determine_play() -> None:
+    play = determine_play('A X')
+    assert play == 'scissors'
+    play = determine_play('A Y')
+    assert play == 'rock'
+    play = determine_play('A Z')
+    assert play == 'paper'
+
+def test_traverse_file_pt_one() -> None:
+    assert rounds1 == traverse_file_pt_one("exampleInput.txt")
+
+
+def test_traverse_file_pt_two() -> None:
+    assert rounds2 == traverse_file_pt_two("exampleInput.txt")
