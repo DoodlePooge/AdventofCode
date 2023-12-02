b48a1dbf1421cabd7bbffb8a13ada88b439ec219
DoodlePooge <paige.maute@gmail.com>
2023-12-02 13:47:49 -0500

    removed unneeded print statements

--- 2023/Puzzle1/puzzle1.py
+++ 2023/Puzzle1/puzzle1.py
@@ -2,14 +2,11 @@ import re
 
 def find_nums(line: str) -> int:
     legend = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
-    print(line)
     nums = re.findall(r'(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))', line)
-    print(nums)
     if len(nums[0]) > 1:
         nums[0] = legend[nums[0]]
     if len(nums[-1]) > 1:
         nums[-1] = legend[nums[-1]]
-    print(nums)
     print(int(nums[0] + nums[-1]))
     return int(nums[0] + nums[-1])
 
@@ -22,7 +19,6 @@ def traverse_file(fileName: str) -> list[int]:
 
     for line in lines:
         sum += find_nums(line)
-        print(sum)
     return sum
 
 
