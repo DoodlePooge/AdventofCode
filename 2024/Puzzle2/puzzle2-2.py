f16fb1165131732badc1b8178bd5c370cc26a0ba
Paige Maute <paige.maute@gmail.com>
2024-12-03 01:47:29 -0500

    I was trying really hard not to traverse the same arrays over and over but here we are

--- 2024/Puzzle2/puzzle2-2.py
+++ 2024/Puzzle2/puzzle2-2.py
@@ -1,11 +1,30 @@
-def get_diffs(arr: [int]) -> [int]:
-    diffs = []
+def is_safe(arr: [int]) -> bool:
     previous = arr[0]
-    for num in arr[1:]:
-        diffs.append(previous - num)
-        previous = num
-    
-    return diffs
+    increase: bool
+    for val in arr[1:]:
+        diff = previous - val
+        try:
+            if increase and diff < 0:
+                return False
+            if not increase and diff > 0:
+                return False
+        except:
+            if diff != 0:
+                increase = diff > 0
+        if diff > 3 or diff < -3 or diff == 0:
+            return False
+        previous = val
+
+    return True
+
+def dampener(arr: int) -> bool:
+    for i, _ in enumerate(arr):
+        temp = arr.copy()
+        temp.pop(i)
+        if is_safe(temp):
+            return True
+
+    return False
 
 def traverse_file(fileName: str) -> [[int]]:
     file = open(fileName)
@@ -24,9 +43,11 @@ def main():
     sum = 0
 
     for report in reports:
-        print(report)
-        diffs = get_diffs(report)
-        print(diffs)
+        if is_safe(report):
+            sum += 1
+        else:
+            if dampener(report):
+                sum += 1
 
 
     print(sum)
